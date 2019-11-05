5cb657b7075864758649abd6aa1ec03ced642b26
Craig van Heerden <craig@codehesion.co.za>
2019-11-05 09:54:59 +0200

    Fixed ios createTransactionWithToken method name

--- ios/PeachMobile.m
+++ ios/PeachMobile.m
@@ -7,7 +7,7 @@
 RCT_EXTERN_METHOD(initPaymentProvider:(NSString *)mode)
 RCT_EXTERN_METHOD(setUrlScheme:(NSString *)urlScheme)
 RCT_EXTERN_METHOD(createTransaction:(NSString *)checkoutID paymentBrand:(NSString *)paymentBrand cardHolder:(NSString *)cardHolder cardNumber:(NSString *)cardNumber cardExpiryMonth:(NSString *)cardExpiryMonth cardExpiryYear:(NSString *)cardExpiryYear cardCVV:(NSString *)cardCVV resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(createTransaction:(NSString *)checkoutID paymentBrand:(NSString *)paymentBrand tokenID:(NSString *)tokenID cardCVV:(NSString *)cardCVV resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(createTransactionWithToken:(NSString *)checkoutID paymentBrand:(NSString *)paymentBrand tokenID:(NSString *)tokenID cardCVV:(NSString *)cardCVV resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(submitTransaction:
                   (NSDictionary *)transactionDict
                   resolver:(RCTPromiseResolveBlock)resolve
